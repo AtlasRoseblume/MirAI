7899743b26417529e8e4e98b20d3588644dd1056
Atlas Roseblume <atlasroseblume@gmail.com>
2025-02-13 19:03:30 -0500

    Add trigger phrase detection

--- src/core.py
+++ src/core.py
@@ -19,17 +19,53 @@ class MirAI:
         self.tts = TTS(voice_path)
         self.stt = STT()
 
-        self.audio_queue = Queue(maxsize=2)
+        self.audio_queue = Queue(maxsize=12) # Set this to an implausibly high number (60 seconds backlog)
         self.listener_thread = Thread(target=MirAI.audio_listener, args=(self,))
         self.listener_thread.start()
 
         self.listening = True
         self.recording = record_mode
 
+        self.buffer = ""
+        self.start_index = None
+
+        self.wake_strings = ["hello world"]
+        self.end_strings = ["goodbye world"]
+
     def audio_listener(self):
         while self.running:
             audio_clip = self.microphone.record(5)
             self.audio_queue.put_nowait(audio_clip) # Could error (but how?)
+
+    @staticmethod
+    def find_last_occurence(string, substrings):
+        return max((string.lower().rfind(sub.lower()) for sub in substrings), default=-1)
+
+    def find_trigger(self, text: str):
+        self.buffer += text
+        self.buffer += " "
+        print(self.buffer)
+
+        if self.start_index is None:
+            start_match = MirAI.find_last_occurence(self.buffer, self.wake_strings)
+            
+            if start_match != -1:
+                self.start_index = start_match + len('hello world')
+        
+        if self.start_index is not None:
+            end_index = MirAI.find_last_occurence(self.buffer, self.end_strings)
+
+            if end_index != -1:
+                capture = self.buffer[self.start_index:end_index].lstrip("., \n\t").capitalize()
+                print(f"Phrase submit {capture}")
+
+                # TODO: Submit to SLM/LLM Queue
+                # TODO: Disable listening, wait for response finalization
+
+                # Reset State
+                self.buffer = ""
+                self.start_index = None
+
 
     def run(self):
         if self.recording:
@@ -48,13 +84,16 @@ class MirAI:
 
                 if self.listening:
                     text = self.stt.transcribe(audio_clip)
-                    print(text)
+                    self.find_trigger(text)
                 else:
                     self.logger.debug("Discarded, not listening currently!")
 
             except KeyboardInterrupt:
                 self.running = False
                 print("Exiting normally.")
+
+                self.listener_thread.join()
+                print("Listener joined.")
                 break
         
         if self.recording:
